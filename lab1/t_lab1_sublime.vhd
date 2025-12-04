b60660ce15b55bd716dbd0f4d933d298fa3c9827
TomMarianer <tom.marianer@gmail.com>
2025-12-04 14:59:56 +0200

    Fixed assignment to 7 bit from 8 bit

--- lab1/t_lab1_sublime.vhd
+++ lab1/t_lab1_sublime.vhd
@@ -4,14 +4,14 @@ end entity T_THERMOSTAT;
 architecture T_THERMOSTAT_ARCH of T_THERMOSTAT is
 component THERMOSTAT is
 	port (
-		CURRENT_TEMP	: in  bit_vector (7 downto 0);
-		DESIRED_TEMP	: in  bit_vector (7 downto 0);
+		CURRENT_TEMP	: in  bit_vector (6 downto 0);
+		DESIRED_TEMP	: in  bit_vector (6 downto 0);
 		DISPLAY_SELECT	: in  bit;
-		TEMP_DISPLAY	: out bit_vector (7 downto 0)
+		TEMP_DISPLAY	: out bit_vector (6 downto 0)
 	);
 end component THERMOSTAT;
 
-signal T_CURRENT_TEMP, T_DESIRED_TEMP, T_TEMP_DISPLAY	: bit_vector (7 downto 0);
+signal T_CURRENT_TEMP, T_DESIRED_TEMP, T_TEMP_DISPLAY	: bit_vector (6 downto 0);
 signal T_DISPLAY_SELECT									: bit;
 
 begin
@@ -23,8 +23,8 @@ UUT: THERMOSTAT
 		TEMP_DISPLAY	=> T_TEMP_DISPLAY
 	);
 
-T_CURRENT_TEMP		<= "11110000", "10101010" after 5 ns, "00001111" after 10 ns, "00000000" after 15 ns;
-T_DESIRED_TEMP		<= "00001111", "01010101" after 5 ns, "11110000" after 10 ns, "00000000" after 15 ns;
+T_CURRENT_TEMP		<= "1110000", "0101010" after 5 ns, "0001111" after 10 ns, "0000000" after 15 ns;
+T_DESIRED_TEMP		<= "0001111", "1010101" after 5 ns, "1110000" after 10 ns, "0000000" after 15 ns;
 T_DISPLAY_SELECT	<= '0', '1' after 5 ns, '0' after 10 ns;
 
 end architecture T_THERMOSTAT_ARCH;
