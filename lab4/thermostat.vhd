27efaca532fa092a63cef1a91aafb1f361b50aaa
TomMarianer <tom.marianer@gmail.com>
2026-06-11 12:55:46 +0300

    Fixed applying outputs to internal signal instead of registered output

--- lab4/thermostat.vhd
+++ lab4/thermostat.vhd
@@ -151,37 +151,37 @@ begin
             when IDLE =>
                 FURNACE_ON_INT <= '0';
                 AC_ON_INT <= '0';
-                FAN_ON <= '0';
+                FAN_ON_INT <= '0';
 
             when COOL_ON =>
                 FURNACE_ON_INT <= '0';
                 AC_ON_INT <= '1';
-                FAN_ON <= '0';
+                FAN_ON_INT <= '0';
 
             when AC_NOW_READY =>
                 FURNACE_ON_INT <= '0';
                 AC_ON_INT <= '1';
-                FAN_ON <= '1';
+                FAN_ON_INT <= '1';
 
             when AC_DONE =>
                 FURNACE_ON_INT <= '0';
                 AC_ON_INT <= '0';
-                FAN_ON <= '1';
+                FAN_ON_INT <= '1';
                 
             when HEAT_ON =>
                 FURNACE_ON_INT <= '1';
                 AC_ON_INT <= '0';
-                FAN_ON <= '0';
+                FAN_ON_INT <= '0';
 
             when FURNACE_NOW_READY =>
                 FURNACE_ON_INT <= '1';
                 AC_ON_INT <= '0';
-                FAN_ON <= '1';
+                FAN_ON_INT <= '1';
 
             when FURNACE_DONE =>
                 FURNACE_ON_INT <= '0';
                 AC_ON_INT <= '0';
-                FAN_ON <= '1';
+                FAN_ON_INT <= '1';
         end case;
         
     end process;
